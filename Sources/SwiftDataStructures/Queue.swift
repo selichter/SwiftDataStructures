12c0415780e131099e55745a94e3e3e7355d9d3c
slichter <lichter.sarahe@gmail.com>
2021-05-27 20:45:46 -0500

    Added Queue

--- Sources/SwiftDataStructures/Queue.swift
+++ Sources/SwiftDataStructures/Queue.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+struct Queue {
+    var queue: [Int]
+    
+    var getLength: Int {
+        return queue.count
+    }
+    
+    init() {
+        queue = []
+    }
+    
+    mutating func enqueue(item: Int) {
+        queue.append(item)
+    }
+    
+    mutating func dequeue() {
+        if !isEmpty() {
+            queue.removeFirst()
+        }
+    }
+    
+    func isEmpty() -> Bool {
+        return queue.count == 0
+    }
+    
+    func peek() -> Int? {
+        return isEmpty() ? nil : queue[0]
+    }
+    
+}
